a7b31d33170453cf326bb1f398b6f9cdde6874e3
Diego Acosta <diego.acosta@externos.bci.cl>
2026-02-25 16:04:54 -0500

    Swagger - documentacion arquitectura

--- src/test/java/com/liverpool/liverpooltest/LiverpoolTestApplicationTests.java
+++ src/test/java/com/liverpool/liverpooltest/LiverpoolTestApplicationTests.java
@@ -3,30 +3,20 @@ package com.liverpool.liverpooltest;
 import com.liverpool.liverpooltest.infrastructure.adapter.out.persistence.repository.MongoUserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @TestPropertySource(properties = {
-        "spring.cache.type=simple",
+        "spring.cache.type=none",
         "spring.autoconfigure.exclude=" +
                 "org.springframework.boot.mongodb.autoconfigure.MongoAutoConfiguration," +
                 "org.springframework.boot.data.mongodb.autoconfigure.DataMongoAutoConfiguration," +
-                "org.springframework.boot.data.mongodb.autoconfigure.DataMongoRepositoriesAutoConfiguration"
+                "org.springframework.boot.data.mongodb.autoconfigure.DataMongoRepositoriesAutoConfiguration," +
+                "org.springframework.boot.data.redis.autoconfigure.RedisAutoConfiguration," +
+                "org.springframework.boot.data.redis.autoconfigure.RedisRepositoriesAutoConfiguration"
 })
 class LiverpoolTestApplicationTests {
-
-    @TestConfiguration
-    static class TestCacheConfig {
-        @Bean
-        CacheManager cacheManager() {
-            return new ConcurrentMapCacheManager();
-        }
-    }
 
     @MockitoBean
     private MongoUserRepository mongoUserRepository;
